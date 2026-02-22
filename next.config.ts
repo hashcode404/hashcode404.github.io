4ac06941b953abb6bb54ae2c293ed63eddd88882
hashcode404 <pranavps962@gmail.com>
2026-02-22 13:44:46 +0530

    feat: Update application title, remove Next.js base path and asset prefix, and adjust package homepage URL.

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/pranav-portfolio",
-  assetPrefix: "/pranav-portfolio/",
   images: {
     unoptimized: true,
   },
